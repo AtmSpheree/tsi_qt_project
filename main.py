e70b946e491c3730ee705e9a5d6fb7c86d157299
Grigorii Kondrashov <59977105+atmspheree@users.noreply.github.com>
2024-02-10 18:47:42 +0300

    Interim results
    2 more application windows have been added,
    more comments has been added/changed

--- main.py
+++ main.py
@@ -8,18 +8,29 @@ import sqlite3
 from main_constants import DB_PATHS, ICON_PATH
 
 
+# The standard Exception Hook allows you to output possible errors
+# to the console when launching an application during development
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
+# Function for working with the sqlite3 library
+# After connecting to the database connection,
+# it allows you to use the SQL LOWER function without bugs
 def sqlite_lower(value_):
     return value_.lower()
 
 
+# Function for working with the sqlite3 library
+# After connecting to the database connection,
+# it allows you to use the SQL UPPER function without bugs
 def sqlite_upper(value_):
     return value_.upper()
 
 
+# Function for working with the sqlite3 library
+# After connecting to the database connection,
+# it allows you to compare data in case-insensitive SQL queries
 def sqlite_ignore_case_collation(value1_, value2_):
     if value1_.lower() == value2_.lower():
         return 0
@@ -29,23 +40,28 @@ def sqlite_ignore_case_collation(value1_, value2_):
         return 1
 
 
+# PyQT5 Abstract Table Model to represent search history data
 class HistoryDataModel(QtCore.QAbstractTableModel):
     def __init__(self):
         super(HistoryDataModel, self).__init__()
         self.data = [["", ""]]
         self.horizontal_headers = []
 
+    # Setting headers - fields from the database for correct display
     def headerData(self, section, orientation, role=Qt.DisplayRole):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             return self.horizontal_headers[section]
         return super().headerData(section, orientation, role)
 
+    # Setting horizontal headers
     def set_horizontal_headers(self, headers):
         self.horizontal_headers = headers
 
+    # Setting table data
     def set_data(self, data):
         self.data = data
 
+    # Adding row into table model
     def append_row(self, row):
         if self.data == [["", ""]]:
             self.data = []
@@ -53,37 +69,71 @@ class HistoryDataModel(QtCore.QAbstractTableModel):
         else:
             self.data = [row] + self.data
 
+    # Getting different table cell using its index
     def data(self, index, role):
         if role == Qt.DisplayRole:
             return self.data[index.row()][index.column()]
 
+    # Getting current table rows count
     def rowCount(self, index):
         return len(self.data)
 
+    # Getting current table columns (db fields) count
     def columnCount(self, index):
         return len(self.data[0])
 
 
+# Main Window of the PyQT5 program
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
+        # Initialization of all initial features of the QMainWindow class
+        # It's a required setting
         super().__init__()
 
+        # Setting application icon using path
         self.setWindowIcon(QIcon(ICON_PATH))
 
-        self.setupUi(self)
         # uic.loadUi("ui_files/main.ui", self)
+        # The command above is used to load the application interface directly from the UI interface file
+        # Avoids compiling the UI file to py using pyuic5 script
+        # It is used exclusively in development to preview the interface design
+        # The production uses a file compiled using the pyuic5 script,
+        # which is imported and connected as an additional class (Ui_MainWindow) to this one
+        self.setupUi(self)
+
+        # Uploading various code blocks
+        # Their work is described in more detail below
         self.set_functional_abilities()
         self.setup_special_ui()
         self.connect_to_dbs()
 
+    # Connecting to databases using the path specified in the config.json file
     def connect_to_dbs(self):
+        # Connecting to a first database and configuring the functions necessary
+        # for the correct operation of SQL queries
         self.db_first_connection = sqlite3.connect(DB_PATHS["tabFirst"])
         self.db_first_connection.create_function("LOWER", 1, sqlite_lower)
         self.db_first_connection.create_function("UPPER", 1, sqlite_upper)
         self.db_first_connection.create_collation("NOCASE", sqlite_ignore_case_collation)
 
+        # Connecting to a second database and configuring the functions necessary
+        # for the correct operation of SQL queries
+        self.db_second_connection = sqlite3.connect(DB_PATHS["tabSecond"])
+        self.db_second_connection.create_function("LOWER", 1, sqlite_lower)
+        self.db_second_connection.create_function("UPPER", 1, sqlite_upper)
+        self.db_second_connection.create_collation("NOCASE", sqlite_ignore_case_collation)
+
+        # Connecting to a third database and configuring the functions necessary
+        # for the correct operation of SQL queries
+        self.db_third_connection = sqlite3.connect(DB_PATHS["tabThird"])
+        self.db_third_connection.create_function("LOWER", 1, sqlite_lower)
+        self.db_third_connection.create_function("UPPER", 1, sqlite_upper)
+        self.db_third_connection.create_collation("NOCASE", sqlite_ignore_case_collation)
+
+    # Functional configuration of PyQt5 components
+    # Enabling event handling of button clicks, etc.
     def set_functional_abilities(self):
-        # First Table View
+        # Setting up the first model of the history data table
         self.tableViewHistoryFirstModel = HistoryDataModel()
         self.tableViewHistoryFirstModel.set_horizontal_headers(["№", "Термин"])
         self.tableViewHistoryFirst.setTextElideMode(Qt.ElideMiddle)
@@ -92,11 +142,40 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.tableViewHistoryFirst.setWordWrap(True)
         self.tableViewHistoryFirst.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
         self.tableViewHistoryFirst.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
-        # First Search Button
-        self.pushButtonSearchFirst.clicked.connect(self.push_button_search_first_clicked)
 
-    def push_button_search_first_clicked(self):
-        cursor = eval(f"self.db_{self.tabWidgetMain.currentWidget().objectName().lower()[3:]}_connection.cursor()")
+        # Setting up the second model of the history data table
+        self.tableViewHistorySecondModel = HistoryDataModel()
+        self.tableViewHistorySecondModel.set_horizontal_headers(["№", "Термин"])
+        self.tableViewHistorySecond.setTextElideMode(Qt.ElideMiddle)
+        self.tableViewHistorySecond.setModel(self.tableViewHistorySecondModel)
+        self.tableViewHistorySecond.horizontalHeader().sectionResized.connect(
+        self.tableViewHistorySecond.resizeRowsToContents)
+        self.tableViewHistorySecond.setWordWrap(True)
+        self.tableViewHistorySecond.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
+        self.tableViewHistorySecond.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
+
+        # Setting up the third model of the history data table
+        self.tableViewHistoryThirdModel = HistoryDataModel()
+        self.tableViewHistoryThirdModel.set_horizontal_headers(["№", "Термин"])
+        self.tableViewHistoryThird.setTextElideMode(Qt.ElideMiddle)
+        self.tableViewHistoryThird.setModel(self.tableViewHistoryThirdModel)
+        self.tableViewHistoryThird.horizontalHeader().sectionResized.connect(
+        self.tableViewHistoryThird.resizeRowsToContents)
+        self.tableViewHistoryThird.setWordWrap(True)
+        self.tableViewHistoryThird.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
+        self.tableViewHistoryThird.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
+
+        # Connecting the buttons in its corresponding function
+        self.pushButtonSearchFirst.clicked.connect(self.push_button_search_clicked)
+        self.pushButtonSearchSecond.clicked.connect(self.push_button_search_clicked)
+        self.pushButtonSearchThird.clicked.connect(self.push_button_search_clicked)
+
+    # The function of searching for data from the first database when pressing connected button
+    # It is implemented using eval and exec,
+    # which allows it to be used repeatedly for different databases
+    def push_button_search_clicked(self):
+        order_name = self.tabWidgetMain.currentWidget().objectName()[3:]
+        cursor = eval(f"self.db_{order_name.lower()}_connection.cursor()")
         value = eval(f"self.lineEditSearch{self.tabWidgetMain.currentWidget().objectName()[3:]}.text()")
         query = f'SELECT terms.id, terms.term FROM abstractions JOIN terms ON abstractions.term_id = terms.id ' \
                 f'WHERE LOWER(abstractions.abstraction) = LOWER("{value}")'
@@ -109,18 +188,31 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                  f".model().layoutChanged.emit()")
             exec(f"self.tableViewHistory{self.tabWidgetMain.currentWidget().objectName()[3:]}"
                  f".resizeRowsToContents()")
-            self.textBrowserInfoFirst.setText(data[1])
-            self.labelErrorFirst.setText("")
+            exec(f'self.textBrowserInfo{order_name}.setText(data[1])')
+            exec(f'self.labelError{order_name}.setText("")')
         else:
-            self.labelErrorFirst.setText("Термина с таким названием нет в базе.")
+            exec(f'self.labelError{order_name}.setText("Термина с таким названием нет в базе.")')
 
+    # Setting specific interface settings that QtDesigner does not allow you to implement
     def setup_special_ui(self):
         self.labelSelectCourse.setMargin(10)
 
 
+# The condition is a stub for running the application script,
+# when it is a directly executable file
+# (the startup is eliminated if it is imported as a module etc.)
 if __name__ == '__main__':
+    # Setting exception hook
     sys.excepthook = except_hook
+
+    # Creating application variable
     app = QApplication(sys.argv)
+
+    # Creating an instance of the main application window class
     ex = MainWindow()
+
+    # Showing the main window to the user
     ex.show()
+
+    # Completion of the script after the user closes the application
     sys.exit(app.exec_())
--- main_constants.py
+++ main_constants.py
@@ -2,5 +2,7 @@ import os
 
 
 DIRNAME = os.path.abspath(os.curdir)
-DB_PATHS = {"tabFirst": os.path.join(DIRNAME, "databases/db_first.db")}
+DB_PATHS = {"tabFirst": os.path.join(DIRNAME, "databases/db_first.db"),
+            "tabSecond": os.path.join(DIRNAME, "databases/db_second.db"),
+            "tabThird": os.path.join(DIRNAME, "databases/db_third.db")}
 ICON_PATH = os.path.join(DIRNAME, "icon.png")
